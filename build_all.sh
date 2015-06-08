f3818989c79dd4e53a9a95ac5ccc623991f8ca7a
Alan Franzoni <username@franzoni.eu>
2015-06-08 21:06:52 +0200

    Enh: add fedora 22 config

--- build_all.sh
+++ build_all.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 set -e
 pushd epel-5-x86-64
-docker build -t epel5dev .
+docker build --pull -t epel5dev .
 popd
 
 pushd epel-6-x86-64
-docker build -t epel6dev .
+docker build --pull -t epel6dev .
 popd
 
 pushd epel-7-x86-64
-docker build -t epel7dev .
+docker build --pull -t epel7dev .
 popd
 
 pushd fedora-20-x86-64
-docker build -t fedora20dev .
+docker build --pull -t fedora20dev .
 popd
 
 pushd fedora-21-x86-64
-docker build -t fedora21dev .
+docker build --pull -t fedora21dev .
+popd
+
+pushd fedora-22-x86-64
+docker build --pull -t fedora22dev .
 popd
 
 pushd fedora-rawhide-x86-64
-docker build -t fedorarawhidedev .
+docker build --pull -t fedorarawhidedev .
 popd
