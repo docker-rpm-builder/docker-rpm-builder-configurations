a62c2291fa2b13b4c1c62136ada217a72569f578
Alan Franzoni <username@franzoni.eu>
2015-09-22 11:10:32 +0200

    Fix: we can't rely on EPEL groups since they are officialy broken?

--- build_all.sh
+++ build_all.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 set -e
 pushd epel-5-x86-64
-docker build --pull -t epel5dev .
+docker build --no-cache --pull -t epel5dev .
 popd
 
 pushd epel-5-i386
-docker build --pull -t epel5i386dev .
+docker build --no-cache --pull -t epel5i386dev .
 popd
 
 pushd epel-6-x86-64
-docker build --pull -t epel6dev .
+docker build --no-cache --pull -t epel6dev .
 popd
 
 pushd epel-7-x86-64
-docker build --pull -t epel7dev .
+docker build --no-cache --pull -t epel7dev .
 popd
 
 pushd fedora-20-x86-64
-docker build --pull -t fedora20dev .
+docker build --no-cache --pull -t fedora20dev .
 popd
 
 pushd fedora-21-x86-64
-docker build --pull -t fedora21dev .
+docker build --no-cache --pull -t fedora21dev .
 popd
 
 pushd fedora-22-x86-64
-docker build --pull -t fedora22dev .
+docker build --no-cache --pull -t fedora22dev .
 popd
 
 pushd fedora-rawhide-x86-64
-docker build --pull -t fedorarawhidedev .
+docker build --no-cache --pull -t fedorarawhidedev .
 popd
